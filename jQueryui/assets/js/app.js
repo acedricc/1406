3612ddcb1c5c4d47359807dde7766fe1d3d26bb9
acedricc <cedric.akakpo@lepoles.org>
2022-06-14 16:52:42 +0200

    1406

---
+++ jQueryui/assets/js/app.js
@@ -0,0 +1,13 @@
+$(function(){
+//* carousel
+var slideImg = [
+    "https://cdn.pixabay.com/photo/2022/06/03/20/15/sunset-7240788_960_720.jpg",
+    "https://cdn.pixabay.com/photo/2013/07/18/20/26/sea-164989_960_720.jpg",
+    "https://cdn.pixabay.com/photo/2015/10/30/20/13/sunrise-1014712_960_720.jpg",
+    "https://cdn.pixabay.com/photo/2016/07/30/00/03/winding-road-1556177_960_720.jpg",
+    "https://cdn.pixabay.com/photo/2015/12/01/20/28/road-1072821_960_720.jpg"
+];
+
+var slider = new MySlider($("#slider"),slideImg);
+})
+
